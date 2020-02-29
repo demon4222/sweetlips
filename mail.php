99c292935b2ae00e4b19389ef5b10d9a843cebcc
Dima Osadchuk <dima12osadchuk@gmail.com>
2020-02-29 21:58:12 +0200

    first commit

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$title = $_POST['title'];
+$message = $_POST['name'];
+
+$to = "sweetlipsua@gmail.com";
+$subject = "Форма дистрибьюторов на sweetlips.com.ua";
+$body = "От: $email\nИмя: $name\nТема: $title\n\nСообщение: $message";
+
+$a = mail($to, $subject, $message);
+
+echo "<script type='text/javascript'>window.top.location='https://36cc4bdd.ngrok.io';</script>";
