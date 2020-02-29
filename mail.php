16091245c3a092a545e83ef340af6f0419a71e2e
Dima Osadchuk <dima12osadchuk@gmail.com>
2020-03-01 01:11:23 +0200

    mail php fix

--- mail.php
+++ mail.php
@@ -2,12 +2,12 @@
 $name = $_POST['name'];
 $email = $_POST['email'];
 $title = $_POST['title'];
-$message = $_POST['name'];
+$message = $_POST['message'];
 
 $to = "sweetlipsua@gmail.com";
 $subject = "Форма дистрибьюторов на sweetlips.com.ua";
 $body = "От: $email\nИмя: $name\nТема: $title\n\nСообщение: $message";
 
-$a = mail($to, $subject, $message);
+$a = mail($to, $subject, $body);
 
 echo "<script type='text/javascript'>window.top.location='http://sweetlips.com.ua';</script>";
